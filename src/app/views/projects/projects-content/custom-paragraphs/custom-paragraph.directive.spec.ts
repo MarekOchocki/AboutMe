9fa462d95886df3848c6ead2fcaa425487b4831d
Xeraserafinapat <marcopolo97@vp.pl>
2023-10-22 21:59:11 +0200

    remove unused test files

--- src/app/views/projects/projects-content/custom-paragraphs/custom-paragraph.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CustomParagraphDirective } from './custom-paragraph.directive';
-
-describe('CustomParagraphDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CustomParagraphDirective();
-    expect(directive).toBeTruthy();
-  });
-});
